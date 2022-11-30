a47cc98daaa4d553076e1ecfcc2e6fbf5faeeb4e
bara2brh <bara1brh@gmail.com>
2022-11-30 20:03:35 +0200

    .

--- lib/resetResult.php
+++ lib/resetResult.php
@@ -4,11 +4,9 @@ $sid = $_POST['sid'];
 $exid = $_POST['exid'];
 if(isset($_POST['sid']) && isset($_POST['exid']))
 {
-    $sql="UPDATE testsresult Set result = '0' Where stuid = $sid AND testid = $exid";
+    $sql="UPDATE testsresult Set result = '0',note = '' Where stuid = $sid AND testid = $exid";
     $result=$link->query($sql);
-    while($row=$result->fetch_assoc()){
-    $data[]=$row;
-    }
+
 }
 else
 echo'error';
