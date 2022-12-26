86b4e574ee6f233aae6af1a31f998b2f628b01c1
bara2brh <bara1brh@gmail.com>
2022-12-26 21:58:16 +0200

    video add done

--- phpf/uploadtest.php
+++ phpf/uploadtest.php
@@ -6,11 +6,16 @@ header('Access-Control-Allow-Methods: GET, POST');
 header("Access-Control-Allow-Headers: X-Requested-With");
 $audio = $_POST['file'];
 $name = $_POST['name'];
+$type=$_POST['type'];
 $user = get_current_user();
 $file = base64_decode($audio);
 file_put_contents($name, $file);
-//C:/Users/sami/AndroidStudioProjects/Avocadbmt-main/audio/$name
-//C:/Users/$user/Documents/GitHub/Avocadbmt-main/audio/$name
-rename ($name,"C:/Users/sami/AndroidStudioProjects/Avocadbmt-main/audio/$name");
+if ($type=='v')
+{
+    rename ($name,"C:/Users/$user/Documents/GitHub/Avocadbmt-main/assets/faceVideos/$name");
+
+}
+else
+rename ($name,"C:/Users/$user/Documents/GitHub/Avocadbmt-main/audio/$name");
 echo 'upload is finished';
 ?>
